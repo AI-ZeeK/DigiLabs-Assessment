546e83854c46434a5cf578e52aa094f96af1c948
Isaac Williams <isaaciyaye174@gmail.com>
2023-01-06 13:37:53 +0100

    New one

--- backend/dist/config/db.js
+++ backend/dist/config/db.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(`${process.env.MONGO_URI}`, {
+            useNewUrlParser: true,
+        });
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.italic.underline);
     }
     catch (error) {
--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI!);
+		const conn: any = await mongoose.connect(`${process.env.MONGO_URI}`, {
+			useNewUrlParser: true,
+		});
 		console.log(
 			`MongoDB Connected: ${conn.connection.host}`.cyan.italic.underline
 		);
